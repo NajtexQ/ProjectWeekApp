d2b77140debb36635552d108a6ba12e747aeeef7
Najtex <timi.kompi@gmail.com>
2022-04-21 10:00:24 +0200

    Components setup

---
+++ frontend/src/components/Posts.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import PostCard from "./PostCard";
+
+export default function Posts() {
+    return (
+        <main>
+
+            <div className="album py-5 bg-light">
+                <div className="container">
+
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+                        <PostCard />
+                        
+                    </div>
+                </div>
+            </div>
+
+        </main>
+    )
+}
