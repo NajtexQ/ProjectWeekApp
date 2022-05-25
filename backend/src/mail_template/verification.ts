f4fb9048ed06d0cd9dcbf5924d916f6c2c9122f1
Najtex <timi.kompi@gmail.com>
2022-05-25 23:37:08 +0200

    Mail verification html template setup

--- backend/src/mail_template/verification.ts
+++ backend/src/mail_template/verification.ts
@@ -1,15 +1,19 @@
+import { readFileSync } from 'fs';
 import { transporter } from 'src/MailService';
 
 const verifyLink = "http://localhost:3000/auth/verify/";
 
 async function Verification(email, uuid) {
 
+    // Read HTML file
+    const mailContent = await readFileSync('./src/mail_template/verificationMail.html', 'utf8');
+
     await transporter.sendMail({
         from: '"The Wall" <no-reply@technajt.eu>',
         to: email,
         subject: "Account verification",
         text: "Verify your account by clicking on the following link: " + verifyLink + uuid,
-        html: "Verify your account by clicking on the following link: " + verifyLink + uuid,
+        html: mailContent.replace('{{verifyLink}}', verifyLink + uuid)
     },
         (error, info) => {
             if (error) {
