ba7d2ae3b969eb65a334e61c77ad2dd151cc907d
Najtex <timi.kompi@gmail.com>
2022-05-26 18:05:00 +0200

    Edited mail message

--- backend/src/mail_template/verification.ts
+++ backend/src/mail_template/verification.ts
@@ -6,14 +6,16 @@ const verifyLink = "http://localhost:3000/auth/verify/";
 async function Verification(email, uuid) {
 
     // Read HTML file
-    const mailContent = await readFileSync('./src/mail_template/verificationMail.html', 'utf8');
+    const rawMailContent = await readFileSync('./src/mail_template/verificationMail.html', 'utf8');
+    
+    const mailContent = rawMailContent.split('{{verifyLink}}').join(verifyLink + uuid);
 
     await transporter.sendMail({
         from: '"The Wall" <no-reply@technajt.eu>',
         to: email,
         subject: "Account verification",
         text: "Verify your account by clicking on the following link: " + verifyLink + uuid,
-        html: mailContent.replace('{{verifyLink}}', verifyLink + uuid)
+        html: mailContent
     },
         (error, info) => {
             if (error) {
