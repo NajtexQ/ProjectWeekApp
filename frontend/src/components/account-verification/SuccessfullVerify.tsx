ee11d6eab8af368f2708fb64c4951c54b474901a
Najtex <timi.kompi@gmail.com>
2022-05-27 08:51:00 +0200

    Account verification setup components

---
+++ frontend/src/components/account-verification/SuccessfullVerify.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PageTitle from "../PageTitle";
+
+export default function SuccessfullVerify() {
+  return (
+    <div>
+      <PageTitle title="Your account has been verified!" />
+    </div>
+  );
+}
--- frontend/src/pages/AccountVerification.tsx
+++ frontend/src/pages/AccountVerification.tsx
@@ -1,16 +1,22 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import SuccessfullVerify from "../components/account-verification/SuccessfullVerify";
+import VerifyRequest from "../components/account-verification/VerifyRequest";
 import PageTitle from "../components/PageTitle";
 import { URL } from "../constants";
+import { useGlobalContext } from "../context/GlobalContext";
 
 
 export default function AccountVerification() {
+
+    const { isLoggedIn } = useGlobalContext();
 
     const [isVerified, setIsVerified] = useState(false);
 
     const { uuid } = useParams();
 
+    const navigate = useNavigate();
 
     const verifyAccount = async () => {
 
@@ -24,13 +30,16 @@ export default function AccountVerification() {
     }
 
     useEffect(() => {
-
-        {uuid && verifyAccount();}
+        if (!isLoggedIn) {
+            {uuid && verifyAccount();}
+            return;
+        }
     }, [])
 
     return (
         <div>
-            <PageTitle title="Your account has been verified!" />
+            {isLoggedIn && <SuccessfullVerify />}
+            {!isLoggedIn && uuid && <VerifyRequest />}
         </div>
     );
 }
